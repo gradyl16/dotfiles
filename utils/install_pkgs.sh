6bd8743a458b00e475b676d610236b08c052f36a
Dylen Greenenwald <dgree21@illinois.edu>
2026-07-04 00:42:52 -0500

    feat: dots helper script
    helps install dependencies on arch linux

---
+++ utils/install_pkgs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Installs every package in deps.txt that is not already installed.
+
+for pkg in $(<pkgs.txt); do
+	if ! (pacman -Q | grep ${pkg} >/dev/null); then
+		paru -S ${pkg} --noconfirm
+	fi
+done
