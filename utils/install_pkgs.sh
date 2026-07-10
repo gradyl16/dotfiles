30470604d38a62119631046dc005b273bc1c6ccb
Dylen Greenenwald <dgree21@illinois.edu>
2026-07-09 19:52:36 -0500

    feat: helper pkg installer script

--- utils/install_pkgs.sh
+++ utils/install_pkgs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Installs every package in deps.txt that is not already installed.
+# Installs every package in pkgs.txt that is not already installed.
 
 for pkg in $(<pkgs.txt); do
 	if ! (pacman -Q | grep ${pkg} >/dev/null); then
